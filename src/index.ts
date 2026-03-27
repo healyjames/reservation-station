3d4f9df0f96496820393a102a3db7ed4c1893546
James <healyjames97@gmail.com>
2026-03-27 20:37:16 +0000

    cleaned up entry point

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import app from './app';
 
-export default {
-	async fetch(request, env) {
-		const { pathname } = new URL(request.url);
-
-		if (pathname === '/api/beverages') {
-			// If you did not use `DB` as your binding name, change it here
-			// const { results } = await env.maximum_bookings_db.prepare('SELECT * FROM Customers WHERE CompanyName = ?').bind('Bs Beverages').run();
-			const { results } = await env.maximum_bookings_db.prepare('SELECT * FROM Reservations').run();
-			return Response.json(results);
-		}
-
-		return new Response('Call /api/beverages to see everyone who works at Bs Beverages');
-	},
-} satisfies ExportedHandler<Env>;
+export default app;
